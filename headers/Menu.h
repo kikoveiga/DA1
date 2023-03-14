876cdcd80dbb5874c44184e1671b969d54bf1a0c
José Francisco Veiga <veigajosefrancisco@gmail.com>
2023-03-14 11:20:33 +0000

    Added the Menu Class

---
+++ headers/Menu.h
@@ -0,0 +1,12 @@
+//
+// Created by kikoveiga on 14-03-2023.
+//
+
+#ifndef DA1_MENU_H
+#define DA1_MENU_H
+
+class Menu {
+
+};
+
+#endif //DA1_MENU_H
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,7 +1,3 @@
 //
 // Created by kikoveiga on 14-03-2023.
 //
-
-int main() {
-
-}
